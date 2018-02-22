f852ec2aff0618554c09aa636e8557622b131b65
kcwoolverton <kcwoolverton@gmail.com>
2018-02-22 13:43:50 -0800

    removed extraneous code

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,2 @@
 module SessionsHelper
-  def not_found
-    render 'sessions/unauthorized'
-  end
-
-  def proper_user(user)
-    if session[:user] != current_user
-      not_found
-    end
-  end
 end
