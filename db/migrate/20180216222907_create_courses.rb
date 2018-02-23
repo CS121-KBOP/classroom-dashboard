6614783e59dd39561819094952afa14f92911616
kcwoolverton <kcwoolverton@gmail.com>
2018-02-22 18:25:27 -0800

    cleaned up files, made indentation consistent

--- db/migrate/20180216222907_create_courses.rb
+++ db/migrate/20180216222907_create_courses.rb
@@ -1,11 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[5.1]
   def change
     create_table :courses do |t|
-      t.string :title
-      t.string :code
-      t.references :user, foreign_key: true
+        t.string :title
+        t.string :code
+        t.references :user, foreign_key: true
 
-      t.timestamps
+        t.timestamps
     end
   end
 end
