8201066d32c6f2d5ccfa1a5d2e710ba7ba4c9f3f
kcwoolverton <kcwoolverton@gmail.com>
2018-02-22 18:55:07 -0800

    cleaned up some more files

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,15 +1,16 @@
 class Student < ApplicationRecord
     belongs_to :course
 
+    # credit to https://rubyplus.com/articles/4131-Import-Records-from-CSV-and-Excel-in-Rails-5 for this!
     def self.import(file, course)
         spreadsheet = Roo::Spreadsheet.open(file.path)
         header = spreadsheet.row(1)
-        (2..spreadsheet.last_row).each do |i|
-            row = Hash[[header, spreadsheet.row(i)].transpose]
-            student = find_by_id(row["id"]) || new
+        (2..spreadsheet.last_row).each do |row_num|
+            row = Hash[[header, spreadsheet.row(row_num)].transpose]
+            student = Student.new
             student.attributes = row.to_hash
             student.course_id = course.id
-            student.save!
+            student.save
         end
     end
 end
