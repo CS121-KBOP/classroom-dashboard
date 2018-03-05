89acff31f59a38f9919a86dca930f388f5684348
kcwoolverton <kcwoolverton@gmail.com>
2018-03-04 22:16:10 -0800

    Stopped a nil student from being created each time you upload images

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -61,15 +61,31 @@ class StudentsController < ApplicationController
         end
     end
 
+    # handles batch upload for students
+    # The name of the file (minus the file extension, hopefully) will be the name of the student
     def import
         @user = User.find(params[:user_id])
         proper_user(@user)
         @course = @user.courses.find(params[:course_id])
+        # Create a new student
         student = Student.new
         student.course_id = @course.id
         student.portrait = params[:file]
+        # The name of the student is the name of the image
         student.name = student.portrait_file_name
-        student.save
+        # Check that an image was actually grabbed my ensuring name is not empty or nil
+        if student.name != "" and student.name != nil
+            # Replace all underscores with spaces, as file names convert spaces to underscores
+            if student.name.include? '_'
+                student.name.gsub!('_', ' ')
+            end
+            # Get rid of the file extension. There is likely a better, more general way to do this,
+            # but for simple applications this brute force way works
+            if student.name.include? ".pdf" or student.name.include? ".jpg" or student.name.include? ".png" or student.name.include? ".JPG"
+                student.name = student.name[0..-5]
+            end
+            student.save
+        end
 
         redirect_to user_course_path(@user, @course)
     end
