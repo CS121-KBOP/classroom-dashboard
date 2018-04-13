35dbc3fb9571c71f3a8db73acf2223d9fae77f36
Loaner <Loaner@WL-219-125.WPA.Claremont.Edu>
2018-04-12 21:01:48 -0700

    made it so creating a new submission for a user overwrites edits any previous submissions if they exist

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -15,13 +15,25 @@ class SubmissionsController < ApplicationController
     def create
         @assignment = Assignment.find(unhashTagtoID(params[:access_tag]))
         @student = @assignment.course.students.find(params[:student_id])
-        @submission = @assignment.submissions.create(:answer => params[:answer], :student_id => params[:student_id])
-        logger.debug "New submission: #{@submission.attributes.inspect}"
-        if @submission.save
-            render(json:  {status: "ok"})
+        # if this student has already submitted for this assignment
+        if @assignment.submissions.exists?(:student_id => params[:student_id].to_i)
+            # find and update that record
+            @submission = @assignment.submissions.where(:student_id => params[:student_id].to_i)
+            if @submission.update(:answer => params[:answer])
+                render(json:  {status: "ok"})
+            else
+                render(json:  {status: "failure"})
+            end
         else
-            render(json:  {status: "failure"})
+            # create a new assignment
+            @submission = @assignment.submissions.create(:answer => params[:answer], :student_id => params[:student_id].to_i)
+            if @submission.save
+                render(json:  {status: "ok"})
+            else
+                render(json:  {status: "failure"})
+            end
         end
+        
     end
 
     def destroy
