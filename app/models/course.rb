a287320587f6aaeb9e04d02fc80de9cfd9d7b063
kcwoolverton <kcwoolverton@gmail.com>
2018-04-14 19:38:54 -0700

    Added year, semester, and section fields, and changed the course index order to take this into account

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
     has_many :students, dependent: :destroy
     has_many :assignments, dependent: :destroy
     has_many :polls, dependent: :destroy
+    validates :semester, presence: true,
+                         length: { minimum: 3, maximum: 10 }
+    validates :code, presence: true,
+                     length: { minimum: 3, maximum: 10 }
 end
